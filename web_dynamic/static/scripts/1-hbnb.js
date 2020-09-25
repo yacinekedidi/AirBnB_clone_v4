c46b01ebf3796c252002adc0aa9133051e5fa556
yacinekedidi <kedidiyacine@gmail.com>
2020-09-25 14:11:54 +0100

    second

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(function() {
+	let dict_ids = {};
+	
+	$('div.amenities input').change(function() {
+		if ($(this).checked)
+		{
+			dict_ids[$(this).attr('data-id')] = $(this).attr('data-name');
+		}
+		else { delete dict_ids[$(this).attr('data-id')]; }
+
+		$("DIV.amenities h4").text(Object.values(dict_ids).join(', ') || '&nbsp;');		
+	});
+	
+});
