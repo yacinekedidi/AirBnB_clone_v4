f03a8f69a3d00186aa78d51225adaa0eedbb25bb
kefitaib <taibkefi@yahoo.fr>
2020-09-26 05:21:06 -0700

    task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(function () {
+  const dictIds = {};
+
+  $('div.amenities input').change(function () {
+    if (this.checked) {
+      dictIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else { delete dictIds[$(this).attr('data-id')]; }
+
+    $('DIV.amenities h4').text(Object.values(dictIds).join(', '));
+  });
+
+  $.ajax({
+  	type: 'GET',
+  	url: 'http://0.0.0.0:5001/api/v1/status/',
+  	complete: function(e, xhr, settings) {
+  		if (e.status === 200) {
+  			$('DIV#api_status').addClass('available');
+  		} else {
+  			$('DIV#api_status').removeClass('available');
+  		}
+  	}
+  });
+});
