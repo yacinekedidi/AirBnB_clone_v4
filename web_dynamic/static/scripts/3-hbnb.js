65962ab9ecf7b8149e6d8257c2a181c09b8252d4
kefitaib <taibkefi@yahoo.fr>
2020-09-26 08:10:27 -0700

    fix

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -21,7 +21,42 @@ $(function () {
   	}
   });
 
-  $.post("http://0.0.0.0:5001/api/v1/places_search/", {}, function( data ) {
-    console.log(data); }, "json");
-
+  $.ajax({
+  type: 'POST',
+  data: '{}',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',.
+  dataType: 'json',
+  contentType: 'application/json'
+  success: function(data) {
+    let s = "";
+    for (const place of data) {
+      s += '<article>';
+      s += '<div class="title_box">';
+      s += '<h2>' + place.name + '</h2>';
+      s += '<div class="price_by_night">$' + place.price_by_night + '</div>';
+      s += '</div>';
+      s += '<div class="information">';
+      s += '<div class="max_guest">' + place.max_guest + 'Guest';
+      if (place.max_guest != 1) {
+        s += 's';
+      }
+      s += '</div>';
+      s += '<div class="number_rooms">' + place.number_rooms + 'Bedroom';
+      if (place.number_rooms != 1) {
+        s += 's';
+      }
+      s += '</div>';
+      s += '<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom';
+      if (place.number_bathrooms != 1) {
+        s += 's';
+      }
+      s += '</div>';
+      s += '</div>';
+      s += '<div class="description">';
+      s += place.description;
+      s += '</div>';
+      s += '</article>';
+    }
+    $('section.places').html(data);
+  }
 });
