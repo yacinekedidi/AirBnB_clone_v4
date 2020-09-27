3a86876e22fb458d925512b7a61f97caccd1060a
yacinekedidi <kedidiyacine@gmail.com>
2020-09-27 12:16:10 +0100

    task-5

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -10,15 +10,15 @@ $(function () {
   });
 
   $.ajax({
-  	type: 'GET',
-  	url: 'http://0.0.0.0:5001/api/v1/status/',
-  	complete: function(e, xhr, settings) {
-  		if (e.status === 200) {
-  			$('DIV#api_status').addClass('available');
-  		} else {
-  			$('DIV#api_status').removeClass('available');
-			//$('.available').css('background-color', '#ff545f');	
-  		}
-  	}
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    complete: function (e, xhr, settings) {
+      if (e.status === 200) {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+        // $('.available').css('background-color', '#ff545f');
+      }
+    }
   });
 });
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -60,4 +60,56 @@ $(function () {
       $('section.places').append(s);
     }
   });
+
+  $('button').click(function () {
+    if (Object.keys(dictIds).length === 0) {
+      $('.places article').remove();
+    } else {
+      const data = { amenities: Object.keys(dictIds) };
+      $.ajax({
+        type: 'POST',
+        data: JSON.stringify(data),
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (d) {
+          if (d.length > 0) {
+            let s = '';
+            for (const place of d) {
+              s += '<article>';
+              s += '<div class="title_box">';
+              s += '<h2>' + place.name + '</h2>';
+              s += '<div class="price_by_night">$' + place.price_by_night + '</div>';
+              s += '</div>';
+              s += '<div class="information">';
+              s += '<div class="max_guest">' + place.max_guest + ' Guest';
+              if (place.max_guest !== 1) {
+                s += 's';
+              }
+              s += '</div>';
+              s += '<div class="number_rooms">' + place.number_rooms + ' Bedroom';
+              if (place.number_rooms !== 1) {
+                s += 's';
+              }
+              s += '</div>';
+              s += '<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom';
+              if (place.number_bathrooms !== 1) {
+                s += 's';
+              }
+              s += '</div>';
+              s += '</div>';
+              s += '<div class="description">';
+              s += place.description;
+              s += '</div>';
+              s += '</article>';
+            } // FOR
+            $('section.places').append(s);
+          }// if
+          else {
+            $('.places article').remove();
+          }
+        } // SUCCESS
+      }); // AJAX
+    } // else
+  });
 });
